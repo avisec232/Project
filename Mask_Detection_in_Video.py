c5721f704f954b93437fa459024658feaaa55c47
avisec232 <abhishekahirwar3588@gmail.com>
2024-11-12 20:54:53 +0530

    Add files via upload

--- Mask_Detection_in_Video.py
+++ Mask_Detection_in_Video.py
@@ -1,5 +1,3 @@
- #https://github.com/Devashi-Choudhary/Face-Mask-Detection
-#python Mask_Detection_in_Video.py
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
